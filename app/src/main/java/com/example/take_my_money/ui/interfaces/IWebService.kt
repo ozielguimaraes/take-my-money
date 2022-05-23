882eed5e2e41ea22e42b83e2497e4e7f73148982
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-05-23 13:45:50 -0300

    feat: API call

--- app/src/main/java/com/example/take_my_money/ui/interfaces/IWebService.kt
+++ app/src/main/java/com/example/take_my_money/ui/interfaces/IWebService.kt
@@ -1,4 +1,45 @@
 package com.example.take_my_money.ui.interfaces
 
+import com.example.take_my_money.ui.models.ModelListCoins
+import com.example.take_my_money.ui.utils.Constants
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface IWebService {
+
+    @GET(Constants.PATH_URL_ALL_COINS)
+    fun getAllCoins(
+        @Query("apikey") apikey: String?
+    ): Call<List<ModelListCoins?>?>
+
+    @GET(Constants.PATH_URL_DETAILS)
+    fun getCoinsDetails(
+        @Path("coin") coin: String?,
+        @Query("apikey") apikey: String?
+    ): Call<List<ModelListCoins?>?>
+
+    @GET(Constants.PATH_URL_IMAGE)
+    fun getImageCoins(
+        @Query("apikey") apikey: String?
+    ): Call<List<ModelListCoins?>?>
+
+    companion object {
+
+        val retrofit: IWebService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            retrofit.create(IWebService::class.java)
+        }
+
+        fun getBaseUrl(): IWebService {
+            return retrofit
+        }
+    }
 }
