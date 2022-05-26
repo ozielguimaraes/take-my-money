426101b466307374a1cfa31654005ddb21f3275c
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-05-26 14:50:56 -0300

    feat:create database

--- app/src/main/java/com/example/take_my_money/ui/repository/RepositoryCoinsDetails.kt
+++ app/src/main/java/com/example/take_my_money/ui/repository/RepositoryCoinsDetails.kt
@@ -5,9 +5,7 @@ import com.example.take_my_money.ui.utils.Constants
 
 class RepositoryCoinsDetails(private val retrofit: IWebService) {
 
-    fun getCoinsDetails(asset_id: String?) {
-        retrofit.getCoinsDetails(asset_id, Constants.API_KEY)
-    }
+    fun getCoinsDetails(assetId: String?) = retrofit.getCoinsDetails(assetId, Constants.API_KEY)
 
     fun getImageCoin() {
         retrofit.getImageCoins(Constants.API_KEY)
