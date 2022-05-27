e3846ccd9899bfd47e52d726d956ecf84f3d7bb5
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-05-27 16:30:14 -0300

    replacing class codes

--- app/src/main/java/com/example/take_my_money/ui/view/CoinListActivity.kt
+++ app/src/main/java/com/example/take_my_money/ui/view/CoinListActivity.kt
@@ -1,13 +1,41 @@
-package com.example.take_my_money.ui.view
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.take_my_money.R
+import com.example.take_my_money.databinding.ActivityMainBinding
+import com.example.take_my_money.ui.view.fragments.CoinsFragment
+import com.example.take_my_money.ui.view.fragments.FavoritesFragment
 
 class CoinListActivity : AppCompatActivity() {
 
+    private val coinsFragment = CoinsFragment()
+    private val favoritesFragment = FavoritesFragment()
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        replaceFragment(coinsFragment)
+        replaceFragment(favoritesFragment)
+
+        binding.btnNavigation.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.ic_coins -> replaceFragment(coinsFragment)
+                R.id.ic_favorites -> replaceFragment(favoritesFragment)
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: CoinsFragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.ic_coins, fragment)
+        transaction.commit()
+    }
+
+    private fun replaceFragment(fragments: FavoritesFragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.ic_favorites, fragments)
+        transaction.commit()
     }
 }
