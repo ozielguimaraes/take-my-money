2d3709cb8c9589acab3c2239cffa5102385ea212
Dety-a <andressa.99.dssouza@gmail.com>
2022-05-27 18:06:28 -0300

    feat: update layout

---
+++ app/src/main/java/com/example/take_my_money/ui/view/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.take_my_money.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.take_my_money.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favorite_activity)
+    }
+}
