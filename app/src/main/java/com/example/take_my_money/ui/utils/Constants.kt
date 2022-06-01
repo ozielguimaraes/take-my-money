6bf31349fad7d8db5cc69df2b6977b4c50922d6f
Dety-a <andressa.99.dssouza@gmail.com>
2022-06-01 15:30:44 -0300

    search field logic, code refinement and layout update

--- app/src/main/java/com/example/take_my_money/ui/utils/Constants.kt
+++ app/src/main/java/com/example/take_my_money/ui/utils/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
         internal const val BASE_URL_IMG = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_512/"
         internal const val PATH_URL_ALL_COINS = "v1/assets?"
         internal const val PATH_URL_DETAILS = "v1/assets/{coin}?"
-        internal const val API_KEY = "BFF5CD2A-62B3-4975-B4D6-FA9E7171B38F"
+        internal const val API_KEY = "B34B7896-41D9-440F-9AB8-01AEB882A127"
         internal const val KEY_INTENT = "123456"
     }
 }
