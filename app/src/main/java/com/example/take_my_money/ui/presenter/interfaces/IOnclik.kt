c43eb7754ba13030dbb31b59911c06dd5e09493e
Wesley Vila Nova <92765775+WesleyVilaNova@users.noreply.github.com>
2022-06-14 10:26:18 -0300

    Delete IOnclik.kt

--- app/src/main/java/com/example/take_my_money/ui/presenter/interfaces/IOnclik.kt
+++
@@ -1,7 +0,0 @@
-package com.example.take_my_money.ui.presenter.interfaces
-
-import com.example.take_my_money.ui.data.dao.CoinEntity
-
-interface IOnclik {
-    fun onClickCoins(coin: CoinEntity)
-}
