e795a3d88b02950c42da1a537b56a1a4b49a367d
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-07-20 16:49:19 -0300

    feat: Test unitary in packet data

---
+++ app/src/main/java/com/example/take_my_money/domain/usecases/UseCaseAllCoin.kt
@@ -0,0 +1,17 @@
+package com.example.take_my_money.domain.usecases
+
+import com.example.take_my_money.domain.abstracts.ICoinAllRepository
+import com.example.take_my_money.domain.entities.CoinDomainEntities
+
+class UseCaseAllCoin(private val iCoinAllRepository: ICoinAllRepository) {
+
+    suspend fun getListCoin(): List<CoinDomainEntities>? = getListCoins()
+
+    private suspend fun getListCoins(): List<CoinDomainEntities>? {
+        val resultCoin = iCoinAllRepository.getAllCoinsRepository()
+        resultCoin?.let {
+            return resultCoin.filter { it.type_is_crypto == 1 }
+        }
+        return null
+    }
+}
