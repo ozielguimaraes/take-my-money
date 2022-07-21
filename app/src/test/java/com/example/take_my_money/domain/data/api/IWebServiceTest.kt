8316ac6b2511762683be55d3f78e294a90b17964
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-07-21 20:19:32 -0300

    feat: test unitary with mock in api

--- app/src/test/java/com/example/take_my_money/domain/data/api/IWebServiceTest.kt
+++ app/src/test/java/com/example/take_my_money/domain/data/api/IWebServiceTest.kt
@@ -2,23 +2,21 @@ package com.example.take_my_money.domain.data.api
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.take_my_money.data.api.IWebService
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.*
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class IWebServiceTest {
 
     @OptIn(DelicateCoroutinesApi::class)
-    private val mainThread = newSingleThreadContext("UI thread")
-
-    private val apiKeyTest = "BFF5CD2A-62B3-4975-B4D6-FA9E7171B38F"
-
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val mainThread = newSingleThreadContext("Ui thread")
+
+    private val apiKeyTest = "BFF5CD2A-62B3-4975-B4D6-FA9E7171B38F"
 
     @Before
     fun setUp() {
@@ -28,7 +26,7 @@ class IWebServiceTest {
     @After
     fun tearDown() {
         Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
-        mainThread.close()
+        /*mainThread.close()*/
     }
 
     @Test
@@ -67,7 +65,7 @@ class IWebServiceTest {
     fun `When call api to return code of protocol`() = runBlocking {
         Assert.assertEquals(
             "h2",
-            IWebService.getBaseUrl().getAllCoins(apiKeyTest).raw().protocol().toString()
+            IWebService.getBaseUrl().getAllCoins(apiKeyTest).raw().protocol.toString()
         ) // Test o retorno de protocolo da api
     }
 
@@ -75,7 +73,7 @@ class IWebServiceTest {
     fun `When call api to return the time of response`() = runBlocking {
         Assert.assertNotEquals(
             0,
-            IWebService.getBaseUrl().getAllCoins(apiKeyTest).raw().receivedResponseAtMillis()
+            IWebService.getBaseUrl().getAllCoins(apiKeyTest).raw().receivedResponseAtMillis
         ) // Test o tempo de resposta em milliseconds
     }
 }
